8f2bdd082b22e6e90cc49229c25435707864c410
jncui <1078094978@qq.com>
2016-02-22 12:07:26 +0800

    init project

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('styles',function (){
+	gulp.src('./src/sass/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./dist/css'))
+})
+
+gulp.task('clean',function(cb){
+ 	del(['./dist'],cb)
+})
+
+gulp.task('default',['clean'],function (){
+	gulp.start('styles','scripts','templates');
+})
+
+gulp.task('watch',function (){
+	gulp.watch('./src/sass/*.scss',['styles']);
+	gulp.watch('./src/coffee/*.coffee',['scripts']);
+	gulp.watch('./views/*.jade',['templates']);
+})
+
+
+
+
+
+
